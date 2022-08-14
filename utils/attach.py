ffc144f4b7b7bc0474497b4b83cb2385f591fd21
satrn8 <verypyc@gmail.com>
2022-08-14 14:58:30 +0300

    commit 08/14

--- utils/attach.py
+++ utils/attach.py
@@ -18,9 +18,9 @@ def add_html(browser):
 
 
 def add_video(browser):
-    video_url = "https://user1:1234@selenoid.autotests.cloud/wd/hub" + browser.driver.session_id + ".mp4"
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
-    allure.attach(html, "video" + browser.driver.session_id, AttachmentType.HTML, '.html')
+    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
 
