9476b1d666ebb16aa3e32471e84661a89f22706d
satrn8 <verypyc@gmail.com>
2022-08-15 00:45:36 +0300

    commit 08/15

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -19,6 +21,7 @@ def pytest_addoption(parser):
 def load_env():
     load_dotenv()
 
+
 @pytest.fixture(scope="function")
 def setup_browser(request):
     browser_version = request.config.getoption("--browser_version")
@@ -35,8 +38,10 @@ def setup_browser(request):
 
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv("LOGIN")
+    password = os.getenv("PASSWORD")
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser = Browser(Config(driver))
